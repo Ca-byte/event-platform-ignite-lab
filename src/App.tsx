7c4399f166778ee011748595adfa081248c8dfff
Caroline Vieira <caroline@fetch.ly>
2022-06-22 19:23:42 +0200

    Visual Project  Structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,14 @@
-import { gql, useQuery } from "@apollo/client"
+import { Event } from "./pages/Event";
 
-const GET_LESSONS_QUERY = gql`
-query {
-  lessons {
-    id
-    title
-
-  }
-}
-`
-
-interface Lesson {
-  id: string;
-  title: string;
-
-}
 export function App() {
 
-const {data} = useQuery(GET_LESSONS_QUERY)
-  console.log(data)
+
 
   return (
-    <ul>
-      {data?.lessons.map((lesson : Lesson ) => {
-        return <li key={lesson.id}>{lesson.title}</li>
-      })}
-
-    </ul>
+    <div>
+     <Event />
+    </div>
+   
   )
 }
 
---
+++ src/components/Lesson.tsx
@@ -0,0 +1,53 @@
+import { CheckCircle, Lock } from 'phosphor-react'
+import { isPast, format } from 'date-fns'
+import  ptBR from 'date-fns/locale/pt-BR'
+
+interface LessonProps {
+    title: string;
+    slug: string;
+    availableAt: Date;
+    type: 'live' | 'class';
+}
+
+export function Lesson(props : LessonProps){
+    
+    const isLessonAvailabe = isPast(props.availableAt);
+    const avalableDateFormatted = format(props.availableAt, "EEEE ' • 'd ' de ' MMMM ' • 'K'h'mm", {
+        locale: ptBR,
+    })
+    return(
+        <a href="#" >
+            <span className="text-gray-300">
+            {avalableDateFormatted}
+            </span>
+            <div className="rounded border border-gray-500 p-4 mt-2">
+                <header className="flex justify-between items-center">
+                    {
+                        isLessonAvailabe ? (
+                            <span className="flex gap-2 justify-between items-center text-sm font-medium text-sky-400">
+                                <CheckCircle size={20}/>
+                                Conteúdo liberado
+                            </span>
+
+                        ):(
+                            <span className="flex gap-2 justify-between items-center text-sm font-medium text-orange-500">
+                                <Lock size={20}/>
+                                Em breve
+                            </span>
+
+                        )
+                    }
+                    
+                    <span className="text-xs font-bold text-purple-500 rounded border border-purple-500 py-[2px] px-2">
+                       {props.type === 'live' ? 'AO VIVO' : 'AULA PRÁTICA' }
+                    </span>
+                </header>
+                <strong className="text-gray-200 mt-5 block">
+                    {props.title}
+                </strong>
+
+            </div>
+        </a>
+       
+    )
+}
